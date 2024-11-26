a1ab5441425f66f87d847b768b4101ebb8086e85
Anil khand <anilkhandthakuri@gmail.com>
2024-11-26 20:29:07 +1100

    Updated FE

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
+import { ToastContainer } from "react-toastify";
+import { AiFillChrome } from "react-icons/ai";
+
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
-      <h1>LMS Web Application</h1>
-    </div>
+    <>
+      <div className="fs-1">
+        <h1>Library Management System</h1>
+        <AiFillChrome />
+      </div>
+      <ToastContainer />
+    </>
   );
 }
 
